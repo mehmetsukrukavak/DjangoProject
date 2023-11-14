9bcaef453b10cc5842b9cfecea516a465d44293a
Mehmet Sukru Kavak <mehmetsukru.kavak07@gmail.com>
2023-11-14 20:11:49 +0300

    dynamic url

--- django_learning_project/first_app/urls.py
+++ django_learning_project/first_app/urls.py
@@ -3,4 +3,7 @@ from . import views
 
 urlpatterns = [
     path("", views.index, name="index"),
+    path("python/", views.python_course, name="python"),
+    path("java/", views.java_course, name="java"),
+    path("<str:courseItem>/", views.choose_course, name="course")
 ]
--- django_learning_project/first_app/views.py
+++ django_learning_project/first_app/views.py
@@ -1,5 +1,20 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+course_dictionary = {
+    "python": "Python Course Page",
+    "java": "Java Course Page",
+    "kotlin": "Kotlin Course Page",
+    "swift": "Swift Course Page",
+}
 def index(request):
     return HttpResponse("This is first Django project, first index")
+
+def python_course(request):
+    return HttpResponse("Python Course Page")
+
+def java_course(request):
+    return HttpResponse("Java Course Page")
+
+def choose_course(request, courseItem):
+    return HttpResponse(course_dictionary[courseItem])
